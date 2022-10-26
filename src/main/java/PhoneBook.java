410b1b60644f1ad808383bd4ffc73f0b68ca6d17
mihanchik94 <mihanchik94@gmail.com>
2022-10-26 20:30:36 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,10 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private final Map<String, String> map = new TreeMap<>();
+
     public boolean add(String name, String number) {
-        return false;
+        return map.putIfAbsent(name, number) == null;
     }
 }
