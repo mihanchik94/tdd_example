7a57fddedaae95ae3b2c9906af940ebfcda30d37
mihanchik94 <mihanchik94@gmail.com>
2022-10-26 21:01:58 +0300

    printAllNames implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class PhoneBook {
     private final Map<String, String> map = new TreeMap<>();
